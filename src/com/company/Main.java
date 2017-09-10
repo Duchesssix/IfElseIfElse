434659b851a746434871c02155ac3274b922f215
YOUR_PUBLIC_NAME <YOUR_PUBLIC@GIT_EMAIL.COM>
2017-09-10 15:11:49 -0500

    Comments added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        double temperature = 92.0;
-        Scanner scanner = new Scanner(System.in);
+        double temperature = 92.0;  //declare and initialize variable
+        Scanner scanner = new Scanner(System.in);  //create scanner to read user input
 
-        System.out.println("Enter a temperature: ");
-        temperature = scanner.nextDouble();
+        System.out.println("Enter a temperature: ");  //prompt user for input
+        temperature = scanner.nextDouble();  //get temperature as a double
+
+        //Use decision making logic to classify temperature
 
         if (temperature < 97.5) {
             System.out.println(temperature + " degrees is a low temperature.");
